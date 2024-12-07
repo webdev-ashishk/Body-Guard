16c7075e83f063ba428a8db3128ba6a1c2afc35c
webdev-ashishk <webdev.ashishk@gmail.com>
2024-12-07 15:53:34 +0530

    chat with us wokring

--- src/components/ChatWithUs.tsx
+++ src/components/ChatWithUs.tsx
@@ -3,7 +3,6 @@ import { ImWhatsapp } from 'react-icons/im';
 
 export default function ChatWithUs() {
   function handleOpenWhatsapp() {
-    console.log('whatsapp opened!');
     const phoneNumber = '+918604600914';
     const url = `whatsapp://send?phone=${phoneNumber}&text=hi`;
 
