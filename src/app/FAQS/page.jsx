4a55465333488c919a3072c6a085dfef81757411
webdev-ashishk <webdev.ashishk@gmail.com>
2024-12-20 14:27:33 +0530

    successfully made toggleAnswer in FAQs

--- src/app/FAQS/page.jsx
+++ src/app/FAQS/page.jsx
@@ -1,27 +1,32 @@
 'use client';
+import { useState } from 'react';
 import _FAQS from '../../data/FAQS.json';
 export default function FAQS() {
-  const handleQA = (e) => {
-    console.log(e.target.innerText);
+  const [activeId, setActiveId] = useState(null);
+  const toggleAnswers = (id) => {
+    setActiveId((prev) => (prev === id ? null : id));
   };
   return (
     <div className="text-white bg-[#1d232a]">
       <h1 className="text-[1.5rem] md:text-[2rem] lg:text-[3rem] p-3 font-extrabold text-center">
         TOP 14 Frequently Asked Questions
       </h1>
-      <ul onClick={handleQA} className="w-11/12 md:w-10/12 lg:w-8/12 mx-auto">
+      <ul className="w-11/12 md:w-10/12 lg:w-8/12 mx-auto">
         {_FAQS?.faqs.map((element) => (
           <li
             key={element.id}
             className="text-2xl bg-[#a87552] p-2 m-4 md:m-8 lg:m-10 rounded-2xl"
+            onClick={() => toggleAnswers(element.id)}
           >
             <p className=" rounded-lg p-2 m-2 flex justify-between">
-              <div>
-                <strong>{element.question}</strong>
-              </div>
-              <div className="text-4xl">+</div>
+              <span>{element.question}</span>
+              <span className="text-4xl">
+                {activeId === element.id ? '-' : '+'}
+              </span>
             </p>
-            <p className="px-2 lg:px-3 py-2 rounded-lg">{element.answer}</p>
+            {activeId === element.id && (
+              <i className="pl-2 m-2"> {element.answer}</i>
+            )}
           </li>
         ))}
       </ul>
