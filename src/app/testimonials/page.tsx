6fc40a56a16df23701100ffb9e02f6eb1bd84df8
webdev-ashishk <webdev.ashishk@gmail.com>
2025-01-04 13:54:59 +0530

    removed height from testimonials

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -2,7 +2,7 @@ import Testimonials from '@/components/HeaderPages/Testimonials';
 
 export default function Page() {
   return (
-    <div className="h-[100vh]">
+    <div className="">
       <Testimonials />
     </div>
   );
