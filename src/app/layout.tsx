1edd3574543853262f7b2bbd2fafcc98c8cc2acf
webdev-ashishk <webdev.ashishk@gmail.com>
2025-01-04 13:04:49 +0530

    removed horizontal scrolls'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="relative">
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#1d232a]`}
       >
